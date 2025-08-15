b119f547ba7d048dd10bf1f1f3c0c2debbd49ce6
Yash Raj <dev.yashraj52@gmail.com>
2025-08-15 20:37:45 +0530

    create working of addBookButton

--- index.js
+++ index.js
@@ -16,3 +16,34 @@ closeButton.addEventListener("click", () => {
 
 //book object
 
+const myLibrary = [];
+
+function Book(bookUrl, bookTitle, bookDescription, bookPrice) {
+    this.bookUrl = bookUrl;
+    this.bookTitle = bookTitle;
+    this.bookDescription = bookDescription;
+    this.bookPrice = bookPrice;
+}
+
+function addBookToLibrary() {
+
+}
+
+//get new book data on addBookButton Click
+
+const addBookButton = document.querySelector("#addBookButton");
+
+addBookButton.addEventListener("click", () => {
+    const url = document.querySelector("#image-url");
+    const title = document.querySelector("#title");
+    const description = document.querySelector("#description");
+    const price = document.querySelector("#price");
+
+    console.log(`${url.value}\n${title.value}\n${description.value}\n${price.value}`);
+
+    url.value = "";
+    title.value = "";
+    description.value = "";
+    price.value = "";
+    // dialog.close();
+});
